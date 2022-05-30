2939c6f1680f6b350c607ff31b045d317f8b7c26
zhangtian <zhangtian@weidian.com>
2022-05-31 00:19:17 +0800

    feat: support babel-plugin-component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 
 const babelImport = require('babel-plugin-import');
+const babelComponent = require('babel-plugin-component');
 const babel = require('@babel/core');
 const importMeta = require('@babel/plugin-syntax-import-meta');
 const resolveBabelConfig = require('./resolve-babel-config.js')
 
+// babel-plugin-import
+// babel-plugin-component
+const pluginList = ['import', 'component']
+
 function usePluginImport(options) {
+  let pluginName = ''
   if (!options) {
     const { plugins = [] } = resolveBabelConfig.sync()
     const importConfig = plugins.find(plugin => {
       if (typeof plugin === "string") {
-        return plugin === 'import'
+        return pluginList.includes(plugin)
       } else if (Array.isArray(plugin)) {
-        return plugin[0] === 'import'
+        return pluginList.includes(plugin[0])
       }
     })
+    pluginName = importConfig && importConfig[0]
     options = importConfig && importConfig[1]
   }
   if (!options) {
@@ -24,7 +31,7 @@ function usePluginImport(options) {
     name: 'vite-plugin-importer-next',
     transform(code, id) {
       if (/\.(?:[jt]sx?|vue)$/.test(id) && !/node_modules\/vite/.test(id)) {
-        const plugins = [importMeta, [babelImport, options]]
+        const plugins = [importMeta, [pluginName === "import" ? babelImport : babelComponent, options]]
         const result = babel.transformSync(code, {
           ast: true,
           plugins,
